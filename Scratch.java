707303e1e5b6f3a3f2c048b46ce59f237b228d02
hariharan19283 <113096845+hariharan19283@users.noreply.github.com>
2022-10-06 00:30:51 -0700

    Add files via upload

---
+++ Scratch.java
@@ -0,0 +1,27 @@
+class Scratch {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int low, high;
+
+        System.out.println("Enter an integer:");
+        low = scan.nextInt();
+        System.out.println("Enter another integer:");
+        high = scan.nextInt();
+
+        if (low > high){
+            int temp = low;
+            low = high;
+            high = temp;
+        }
+
+        System.out.println("\nEVEN numbers between 2 inputs:");
+        for (int i = low ; i <= high ; i++) {
+            
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
